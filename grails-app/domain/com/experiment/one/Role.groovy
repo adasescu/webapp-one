d2df2deb11836cbfdd3723292417ff5c692525e9
adasescu <ada.sescu@hpe.com>
2025-01-30 11:38:21 +0200

    Add code to domains

--- grails-app/domain/com/experiment/one/Role.groovy
+++ grails-app/domain/com/experiment/one/Role.groovy
@@ -2,6 +2,13 @@ package com.experiment.one
 
 class Role {
 
+    String authority
+    String description
+    Boolean editable
+
     static constraints = {
+        authority maxSize: 255
+        description maxSize: 255
+        editable nullable: false, defaultValue: false
     }
 }
--- grails-app/domain/com/experiment/one/User.groovy
+++ grails-app/domain/com/experiment/one/User.groovy
@@ -2,6 +2,13 @@ package com.experiment.one
 
 class User {
 
+    String name
+    Boolean admin
+    Integer loginCount
+    Role role
+
     static constraints = {
+        name maxSize: 255
+        admin nullable: false, defaultValue: false
     }
 }
