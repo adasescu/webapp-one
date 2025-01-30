d1835b56478d2b870b4d347bf0569c5fd82d8a94
adasescu <ada.sescu@hpe.com>
2025-01-30 11:41:54 +0200

    Add bootstrap data

--- grails-app/init/com/experiment/one/BootStrap.groovy
+++ grails-app/init/com/experiment/one/BootStrap.groovy
@@ -1,9 +1,18 @@
 package com.experiment.one
 
+import groovy.transform.CompileStatic
+
+@CompileStatic
 class BootStrap {
 
     def init = { servletContext ->
+        def adminRole = new Role(authority: 'ROLE ADMIN', description: 'Administrator', editable: false).save(failOnError: true)
+        def userRole = new Role(authority: 'ROLE USER', description: 'User', editable: false).save(failOnError: true)
+
+        def adminUser = new User(name: 'admin', admin: true, loginCount: 1, role: adminRole).save(failOnError: true)
+        def userUser = new User(name: 'user', admin: false, loginCount: 1, role: userRole).save(failOnError: true)
     }
+
     def destroy = {
     }
 }
